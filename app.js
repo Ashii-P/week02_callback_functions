b25e6532bfa84d8a1c8c5abaf23152abbee7be01
Aishah Perks <byaishah.altaf@gmail.com>
2025-01-14 15:11:28 +0000

    Done most besides the last part

--- app.js
+++ app.js
@@ -38,3 +38,25 @@ function onAwesomeFailure() {
   console.log("It failed :(");
 }
 myAwesomeFunction(onAwesomeSuccess, onAwesomeFailure);
+
+function add(a, b) {
+  return a + b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function myCalculatorFunction(a, b, operationCallback) {
+  console.log("Doing a calculation using the numbers:", a, b);
+  console.log(
+    "We might have code that saves the result to a log or a database"
+  );
+  return operationCallback(a, b);
+}
+const result = myCalculatorFunction(1, 2, add);
+console.log(result);
+
+const result2 = myCalculatorFunction(2, 3, multiply);
+console.log(result2);
+
+const finalResult = myCalculatorFunction(result, result2, add);
+console.log(finalResult);
